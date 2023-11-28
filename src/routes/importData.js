7555d9530f3fb8653c00959e897a3531d22e3942
JuanJoseQuiroga <juanjo1998_x@hotmail.com>
2023-11-28 19:47:01 -0400

    cleaned code

--- src/routes/importData.js
+++ src/routes/importData.js
@@ -11,32 +11,20 @@ router.get("/importData", (req, res) => {
   fs.createReadStream("TXdataAnt.csv")
     .pipe(csv())
     .on("data", (data) => {
-      //console.log("dataaaaa ::: ", data.Fotografia);
-
-      //data.Fotografia = data.Fotografia.replace(/'/g, '"')
-      /* console.log(
-        " data double quotes modified: ",
-        data.Fotografia.replace(/'/g, '"')
-      ); */
       if (data.Fotografia) {
         imageI = JSON.parse(data.Fotografia.replace(/'/g, '"'));
         imageI.url = "https://adalo-uploads.imgix.net/" + imageI.url;
-        //console.log("parsed imageI: ", imageI);
+
         data.Fotografia = imageI;
       }
       if (data.FotografiaII) {
         imageII = JSON.parse(data.FotografiaII.replace(/'/g, '"'));
         imageII.url = "https://adalo-uploads.imgix.net/" + imageII.url;
-        //console.log("parsed imageII: ", imageII);
         data.FotografiaII = imageII;
       }
-
-      /* const image1Splited = data.Fotografia.split("'");
-      console.log("data splited: ", image1Splited[3]); */
       results.push(data);
     })
     .on("end", () => {
-      //console.log(results);
       res.json(results);
     });
 });
