401f557985f0ec7deca01ba2ab267b230df0506d
JuanJoseQuiroga <juanjo1998_x@hotmail.com>
2023-11-29 12:19:41 -0400

    refactored uploadFile code

--- src/routes/uploadFile.js
+++ src/routes/uploadFile.js
@@ -1,24 +1,6 @@
 const { Router } = require("express");
+const { upload } = require("../utils/upload");
 const router = Router();
-
-const multer = require("multer");
-
-const storage = multer.diskStorage({
-  destination: function (req, file, callback) {
-    callback(null, "src/files");
-  },
-  filename: function (req, file, callback) {
-    const filename = file.originalname;
-    callback(null, filename);
-  },
-});
-
-const upload = multer({
-  storage: storage,
-  limits: {
-    fileSize: 2048576, // 2 Mb
-  },
-});
 
 router.post("/uploadFile", upload.any(), function (req, res) {
   res.json({ message: "upload PDF File" });
