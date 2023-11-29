b0e0fa646e084ef3b9b6a528966c036c7344fd4b
JuanJoseQuiroga <juanjo1998_x@hotmail.com>
2023-11-28 20:13:51 -0400

    refactored code

--- src/routes/importData.js
+++ src/routes/importData.js
@@ -1,32 +1,9 @@
 const { Router } = require("express");
+const { importDataFromCsv } = require("../utils/importDataFromCsv");
 const router = Router();
 
-const csv = require("csv-parser");
-const fs = require("fs");
-const results = [];
-let imageI;
-let imageII;
-
 router.get("/importData", (req, res) => {
-  fs.createReadStream("TXdataAnt.csv")
-    .pipe(csv())
-    .on("data", (data) => {
-      if (data.Fotografia) {
-        imageI = JSON.parse(data.Fotografia.replace(/'/g, '"'));
-        imageI.url = "https://adalo-uploads.imgix.net/" + imageI.url;
-
-        data.Fotografia = imageI;
-      }
-      if (data.FotografiaII) {
-        imageII = JSON.parse(data.FotografiaII.replace(/'/g, '"'));
-        imageII.url = "https://adalo-uploads.imgix.net/" + imageII.url;
-        data.FotografiaII = imageII;
-      }
-      results.push(data);
-    })
-    .on("end", () => {
-      res.json(results);
-    });
+  importDataFromCsv(req, res);
 });
 
 module.exports = router;
