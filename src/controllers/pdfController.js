0f7ea4d9572b3563739649b4825754d9dbcaacbe
FrozstDev <frozstdev@gmail.com>
2023-11-29 16:08:44 -0400

    Add Controller to generate a pdf from multiply data

--- src/controllers/pdfController.js
+++ src/controllers/pdfController.js
@@ -1,39 +1,22 @@
-const hbs = require('handlebars')
-const PuppeteerHTMLPDF = require('puppeteer-html-pdf')
+const createPDF = require('../utils/pdfs/generatePDF')
+const renderTemplate = require('../utils/pdfs/renderTemplate')
 
 const generatePDFController = async (req, res) => {
-  const pdfData = {
-    invoiceItems: [
-      { item: 'Website Design', amount: 5000 },
-      { item: 'Hosting (3 months)', amount: 2000 },
-      { item: 'Domain (1 year)', amount: 1000 }
-    ],
-    invoiceData: {
-      invoice_id: 123,
-      transaction_id: 1234567,
-      payment_method: 'Paypal',
-      creation_date: '04-05-1993',
-      total_amount: 141.5
-    },
-    baseUrl: `${req.protocol}://${req.get('host')}` // http://localhost:3000
-  }
+  const reportes = { reports: [{ name: 'articulo1' }, { name: 'articulo2' }] }
+  const HTML = renderTemplate('template', reportes)
+  const pdfBuffer = await createPDF({ templateHTML: HTML })
+  console.log(pdfBuffer)
 
-  const htmlPDF = new PuppeteerHTMLPDF()
-  htmlPDF.setOptions({ format: 'A4' })
+  // Configurar los encabezados de la respuesta
+  res.setHeader('Content-Type', 'application/pdf')
+  // res.setHeader('Content-Disposition', 'attachment; filename=prueba1.pdf')
 
-  try {
-    const html = await htmlPDF.readFile('src/resources/templates/template.hbs', 'utf8')
-    console.log(html)
-    const template = hbs.compile(html)
-    const content = template(pdfData)
+  // Transmitir los buffers como respuesta
+  res.write(pdfBuffer)
 
-    const pdfBuffer = await htmlPDF.create(content)
-    res.attachment('invoice.pdf')
-    res.end(pdfBuffer)
-  } catch (err) {
-    console.log(err)
-    res.status(500).send(err)
-  }
+  // Finalizar la respuesta
+  res.end()
+  // res.send('Hola')
 }
 
 module.exports = generatePDFController
--- src/utils/pdfs/renderTemplate.js
+++ src/utils/pdfs/renderTemplate.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 const path = require('path')
 
 function renderTemplate (templateName, data) {
-  const htmlFilePath = path.join(__dirname, `../resources/templates/${templateName}.hbs`)
+  const htmlFilePath = path.join(__dirname, `../../resources/templates/${templateName}.hbs`)
   const html = fs.readFileSync(htmlFilePath, 'utf8')
 
   const template = hbs.compile(html)
