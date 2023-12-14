6f574212205ac31b29b67ff07bad86e6d2190219
Carlos <carlos.vargas@assuresoftbolivia.com>
2023-12-13 20:11:47 -0400

    reduciendo el tamaño del pdf

--- src/controllers/pdfController.js
+++ src/controllers/pdfController.js
@@ -1,7 +1,8 @@
 const { handleHttpError, handleHttpErrorCustome } = require('../utils/handlesMessage/handleHttpError')
 const createPDF = require('../utils/pdfs/generatePDF')
 const renderTemplate = require('../utils/pdfs/renderTemplate')
-
+const sharp = require('sharp')
+const request = require('request')
 const generatePDFController = async (req, res) => {
   const reportes = { reports: [{ name: 'articulo1' }, { name: 'articulo2' }] }
   const HTML = renderTemplate('template', reportes)
@@ -25,14 +26,34 @@ const generateOnePDFController = async (req, res) => {
     if (Object.keys(data).length === 0) {
       return handleHttpErrorCustome({ res, message: 'Ingrese el dato de un registro', code: 404 })
     }
-
-    const HTMLContent = renderTemplate('pdfTemplateOne', data)
-    const pdfBuffer = await createPDF({ templateHTML: HTMLContent })
-
-    res.setHeader('Content-Type', 'application/pdf')
-    // res.setHeader('Content-Disposition', 'attachment; filename=prueba1.pdf')
-    res.write(pdfBuffer)
-    res.end()
+    let url = data.Fotografia.url
+    request({ url, encoding: null }, function (error, response, body) {
+      if (!error) {
+        sharp(body).rotate().resize(300, 300).toBuffer().then(function (foto1) {
+          const b64encoded = btoa(String.fromCharCode.apply(null, foto1))
+          const datajpg = 'data:image/jpg;base64,' + b64encoded
+          data.Fotografia.url = datajpg
+        }).then(function () {
+          url = data.FotografiaII.url
+          request({ url, encoding: null }, function (error, response, body) {
+            if (!error) {
+              sharp(body).rotate().resize(200, 200).toBuffer().then(function (foto2) {
+                const b64encoded2 = btoa(String.fromCharCode.apply(null, foto2))
+                const datajpg2 = 'data:image/jpg;base64,' + b64encoded2
+                data.FotografiaII.url = datajpg2
+              }).then(async function () {
+                const HTMLContent = renderTemplate('pdfTemplateOne', data)
+                const pdfBuffer = await createPDF({ templateHTML: HTMLContent })
+                res.setHeader('Content-Type', 'application/pdf')
+                // res.setHeader('Content-Disposition', 'attachment; filename=prueba1.pdf')
+                res.write(pdfBuffer)
+                res.end()
+              })
+            }
+          })
+        })
+      }
+    })
   } catch (error) {
     handleHttpError({ res, error: error.message })
   }
