070fb046ab124b23b57c8fa04bd5d49db106fdcf
Carlos <carlos.vargas@assuresoftbolivia.com>
2024-03-18 01:23:23 -0400

    agregar precio final

--- src/utils/cleanData/cleanData.js
+++ src/utils/cleanData/cleanData.js
@@ -6,6 +6,12 @@ const cleanData = async ({ dataTasacion }) => {
   const image1 = dataTasacion.urlImage1
   const image2 = dataTasacion.urlImage2
 
+  if (dataTasacion.useFormule) {
+    const price = dataTasacion.Va
+    const priceNum = price.toFixed(2)
+    dataTasacion.Va = priceNum
+  }
+
   if (image1) dataTasacion.urlImage1 = await procesarImagen(image1)
   if (image2) dataTasacion.urlImage2 = await procesarImagen(image2)
 
